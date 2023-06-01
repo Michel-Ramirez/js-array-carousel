9cbeaef716579eb9a90ba41a5dcbd23667c2fbc6
Michel-Ramirez <michel.ramirez19@gmail.com>
2023-06-01 16:55:54 +0200

    add js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 console.log('JS OK')
 
 //recupero gli elementi dal dom
-const gallery = document.getElementById('gallery')
+const gallery = document.getElementById('gallery');
+const nextBtn = document.getElementById('next');
+const prevBtn = document.getElementById('prev');
 
 //lista delle immagini da passare al dom
 const listImg = ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'img/05.webp'];
+
