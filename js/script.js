0dbd4c5fa1315c12366b4d285c7e4d6ba1c8dc33
Michel-Ramirez <michel.ramirez19@gmail.com>
2023-06-03 17:07:32 +0200

    fix and finish thumbnails

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ console.log('JS OK')
 const nextBtn = document.getElementById('next');
 const prevBtn = document.getElementById('prev');
 const innerGallery = document.querySelector('.gallery')
-
+const prevImgGallery = document.querySelector('.prev-img')
 //lista delle immagini da passare al 
 const listImg = ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'img/05.webp'];
 
@@ -12,20 +12,20 @@ const listImg = ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'im
 
 let imgElement = '';
 
-//*******/ Ciclo di montaggio degli element /*****//
+//*******/\ | Ciclo di montaggio degli element | \/*****//
 
 for (let i = 0; i < listImg.length; i++){
-    imgElement += `<img src="${listImg[i]}">`;
-    console.log(imgElement)
+    imgElement += `<img src = "${listImg[i]}" >`;
 }
 
 // inseriamo nel dom gli elementi montai
-
 innerGallery.innerHTML = imgElement;
 
 
+/********************* ! | CAROUSEL | ! *********************/
+
 //selezione l'intero nodo di immagini
-const images = document.querySelectorAll('#carousel img');
+const images = document.querySelectorAll('.gallery img');
 
 /*preparo la gestione delle immagini alla quale successivamente andrò a mettere la classe d-block
 seleziono la immagine in posizione 0 per mostrarla immediatamente sulla pagine al caricamento */
@@ -36,45 +36,80 @@ let currentImg = 0;
 
 images[currentImg].classList.add('d-block');
 
+
+/********************* ! | THUMBNAILS | ! *********************/
+
+//Inserisco le thumbnails
+prevImgGallery.innerHTML += imgElement;
+
+// seleziono l'elemnto al quale devo applicare le classi
+
+const thumbImg = document.querySelectorAll('.prev-img img');
+
+let currentThumb = 0;
+
+thumbImg[currentThumb].classList.add('border');
+
+
+/********************* ! | BTN | ! *********************/
+
+
 // Al click su next deve togliere la classe d-block alla immagine e aggiungerla alla img in posizione seguente incrementtando di 1//
 
 nextBtn.addEventListener('click', function(){
-
+    
     //Aggiungo controllo: se non ci sono più immagini stoppa l'incremento
-
-    if (currentImg === images.length -1 ) {
+    
+    if (currentImg === images.length - 1 ) {
         return;
     }
-
+    
     //rimuovo la classe dall'immagine
     images[currentImg].classList.remove('d-block');
+    
+    thumbImg[currentThumb].classList.remove('border');
 
     //INCREMENTO LA POSIZIONE 
-
+    
     currentImg++;
 
+    currentThumb++;
+    
     //aggiungo la classe 
     images[currentImg].classList.add('d-block');
-
+    
+    thumbImg[currentThumb].classList.add('border');
 });
 
 // al click deve fare la stessa cosa di sopra ma decrementando la posizione du currentImg
 prevBtn.addEventListener('click', function(){
-
+    
     //Aggiungo controllo: se non ci sono più immagini stoppo
-
+    
     if (currentImg === 0){
         return;
     }
-
+    
     //rimuovo la classe dall'immagine
     images[currentImg].classList.remove('d-block');
 
-    //INCREMENTO LA POSIZIONE 
+    thumbImg[currentThumb].classList.remove('border');
 
+    
+    //INCREMENTO LA POSIZIONE 
+    
     currentImg--;
+    
+    currentThumb--;
 
     //aggiungo la classe 
     images[currentImg].classList.add('d-block');
 
+    thumbImg[currentThumb].classList.add('border');
+
+    
 });
+
+
+
+
