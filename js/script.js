96e2f1dfaa0abcd49486b7b50f5ffad9b8ef6da6
Michel-Ramire <michel.ramirez19@gmil.com>
2023-06-03 14:59:14 +0200

    Finish carousel

--- js/script.js
+++ js/script.js
@@ -3,9 +3,26 @@ console.log('JS OK')
 //recupero gli elementi dal dom
 const nextBtn = document.getElementById('next');
 const prevBtn = document.getElementById('prev');
+const innerGallery = document.querySelector('.gallery')
 
-// //lista delle immagini da passare al dom
-// const listImg = ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'img/05.webp'];
+//lista delle immagini da passare al 
+const listImg = ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'img/05.webp'];
+
+// Variabile di appoggio per gli elementi da montare 
+
+let imgElement = '';
+
+//*******/ Ciclo di montaggio degli element /*****//
+
+for (let i = 0; i < listImg.length; i++){
+    imgElement += `<img src="${listImg[i]}">`;
+    console.log(imgElement)
+}
+
+// inseriamo nel dom gli elementi montai
+
+innerGallery.innerHTML = imgElement;
+
 
 //selezione l'intero nodo di immagini
 const images = document.querySelectorAll('#carousel img');
@@ -18,9 +35,6 @@ let currentImg = 0;
 // aggiungo la classe d-block alla immagini selezionata 
 
 images[currentImg].classList.add('d-block');
-
-
-//*******/ CICLI /*****//
 
 // Al click su next deve togliere la classe d-block alla immagine e aggiungerla alla img in posizione seguente incrementtando di 1//
 
